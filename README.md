9ab039bd67b328f1d36ae6778310e719a7763267
dkause <d.g.kause@gmail.com>
2023-03-29 21:28:46 +0200

    Update README.md and scripts.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PokeView-App
-A Pokédex application that displays a list and details of Pokémon
+
+A Pokédex application that displays a list and details of Pokémon by loading data from an external API.
+
+## Context
+
+This is app is an exercise to learn programming, in this case Front End  Development using HTML, CSS and Javascript.
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert('Hello World!');
-
-let favoriteFood = 'Quark mit Pellkartoffeln';
-document.write(favoriteFood);
+let pokemonList = [{name:'Butterfree', height: 1.1 , types: ['grass', 'ice', 'electric'] }, {name:'Kakuna', height: 0.6, types: ['psychic', 'fairy', 'grass'] }, {name:'Beedrill', height: 1, types:['ice', 'electric', 'psychic']}];
