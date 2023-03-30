b8639808900f51261ce859b6dd848c509568bb4a
dkause <d.g.kause@gmail.com>
2023-03-30 12:57:59 +0200

    fix code formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [{name:'Butterfree', height: 1.1 , types: ['grass', 'ice', 'electric'] }, {name:'Kakuna', height: 0.6, types: ['psychic', 'fairy', 'grass'] }, {name:'Beedrill', height: 1, types:['ice', 'electric', 'psychic']}];
+let pokemonList = [
+  { name: "Butterfree", height: 1.1, types: ["grass", "ice", "electric"] },
+  { name: "Kakuna", height: 0.6, types: ["psychic", "fairy", "grass"] },
+  { name: "Beedrill", height: 1, types: ["ice", "electric", "psychic"] },
+];
