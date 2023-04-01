8ef746cb86ced3c9aadbbed471eaa183da1d6eb0
dkause <d.g.kause@gmail.com>
2023-04-01 12:18:17 +0200

    fix use document.write

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,18 @@ let pokemonList = [
 // Set loop iterator to zero, get the number of items in the object, iterate it until end of numer of items
 
 for (let i = 0; i < pokemonList.length; i++) {
-
   // if the property height in object pokemonlist is bigger as one
   if (pokemonList[i].height > 1) {
-
     // write in the element with id=pokemonlistItem a <li> with pokemon name and add a span element with text " is a big pokemon" and a closing </li>
-    document.getElementById("pokemonlistItem").innerHTML +=
+    document.write(
       "<li>" +
-      pokemonList[i].name +
-      "<span class='bigPokemon'> is a big pokemon</span>" +
-      "</li>";
+        pokemonList[i].name +
+        "<span class='bigPokemon'> is a big pokemon</span>" +
+        "</li>"
+    );
 
     // for all items smaller as one, write in the same element a <li> with the pokemon name and a closing </li>
   } else {
-    document.getElementById("pokemonlistItem").innerHTML +=
-      "<li>" + pokemonList[i].name + "</li>";
+    document.write("<li>" + pokemonList[i].name + "</li>");
   }
 }
