a9d474bb9258f8ef8da88220057cc1ef9eba306e
dkause <d.g.kause@gmail.com>
2023-04-09 19:54:48 +0200

    update: refactor code, add styles and fonts

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,32 @@
-let pokemonList = [
-  { name: "Butterfree", height: 1.1, types: ["grass", "ice", "electric"] },
-  { name: "Kakuna", height: 0.6, types: ["psychic", "fairy", "grass"] },
-  { name: "Beedrill", height: 1, types: ["ice", "electric", "psychic"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Butterfree", height: 1.1, types: ["grass", "ice", "electric"] },
+    { name: "Kakuna", height: 0.6, types: ["psychic", "fairy", "grass"] },
+    { name: "Beedrill", height: 1, types: ["ice", "electric", "psychic"] },
+  ];
 
-// Set loop iterator to zero, get the number of items in the object, iterate it until end of numer of items
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-for (let i = 0; i < pokemonList.length; i++) {
-  // if the property height in object pokemonlist is bigger as one
-  if (pokemonList[i].height > 1) {
-    // write in the element with id=pokemonlistItem a <li> with pokemon name and add a span element with text " is a big pokemon" and a closing </li>
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height > 1) {
     document.write(
       "<li>" +
-        pokemonList[i].name +
+        pokemon.name +
         "<span class='bigPokemon'> is a big pokemon</span>" +
         "</li>"
     );
-
-    // for all items smaller as one, write in the same element a <li> with the pokemon name and a closing </li>
   } else {
-    document.write("<li>" + pokemonList[i].name + "</li>");
+    document.write("<li>" + pokemon.name + "</li>");
   }
-}
+});
